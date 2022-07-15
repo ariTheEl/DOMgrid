4941b8a98afc3859d8f0452f29e01c4eb1c2f151
Navwojah-Ariko_Eloho <arikoeloho1996@gmail.com>
2022-07-15 16:45:13 +0100

    feat:Create a function for generating a grid

--- script.js
+++ script.js
@@ -1,167 +1,28 @@
+// function promptFunction(){
+//     let numberOfSquares=prompt("Enter the number of squares. ")
+// }
+
+// const gridNumber=document.querySelector('#userEntry')
+// let rows=gridNumber.addEventListener('click', promptFunction)
+
+
+let rows=document.querySelector('#userEntry')
+let rowsNum=rows.getAttribute('value')
+let rowNum= Number(rowsNum)
+
+
 const container=document.querySelector("#container");
+function gridMaker(rowsNum) {
+    for(i=0; i<rowsNum, i++;){
+        const gridRow=document.createElement('div')
+        container.appendChild(gridRow)
+        container.style.display='flex';
+        for (j=0; j<rowsNum.length; j++){
+            let column=document.createElement('span')
+            gridRow.appendChild(column)
+    }
+    }
+    
+}
 
-const divRow=document.createElement('span');
-divRow.classList.add('divrow')
-container.appendChild(divRow);
-
-const divRow2=document.createElement('span');
-divRow2.classList.add('divrow2')
-container.appendChild(divRow2);
-
-
-
-
-
-
-const div1=document.createElement('div');
-const div2=document.createElement('div');
-const div3=document.createElement('div');
-const div4=document.createElement('div');
-const div5=document.createElement('div');
-const div6=document.createElement('div');
-const div7=document.createElement('div');
-const div8=document.createElement('div');
-const div9=document.createElement('div');
-const div10=document.createElement('div');
-const div11=document.createElement('div');
-const div12=document.createElement('div');
-const div13=document.createElement('div');
-const div14=document.createElement('div');
-const div15=document.createElement('div');
-const div16=document.createElement('div'); 
-
-divRow.appendChild(div1);
-divRow.appendChild(div2);
-divRow.appendChild(div3);
-divRow.appendChild(div4);
-divRow.appendChild(div5);
-divRow.appendChild(div6);
-divRow.appendChild(div7);
-divRow.appendChild(div8);
-divRow.appendChild(div9);
-divRow.appendChild(div10);
-divRow.appendChild(div11);
-divRow.appendChild(div12);
-divRow.appendChild(div13);
-divRow.appendChild(div14);
-divRow.appendChild(div15);
-divRow.appendChild(div16);
-
-const divRow1=document.createElement('span');
-divRow1.classList.add('divrow1')
-container.appendChild(divRow1);
-
-const div1_1=document.createElement('div');const div1_2=document.createElement('div');
-const div1_3=document.createElement('div');const div1_4=document.createElement('div');
-const div1_5=document.createElement('div');const div1_6=document.createElement('div');
-const div1_7=document.createElement('div');const div1_8=document.createElement('div');
-const div1_9=document.createElement('div');const div1_10=document.createElement('div');
-const div1_11=document.createElement('div');const div1_12=document.createElement('div');
-const div1_13=document.createElement('div');const div1_14=document.createElement('div');
-const div1_15=document.createElement('div');const div1_16=document.createElement('div'); 
-
-divRow1.appendChild(div1_1);divRow1.appendChild(div1_2);
-divRow1.appendChild(div1_3);divRow1.appendChild(div1_4);
-divRow1.appendChild(div1_5);divRow1.appendChild(div1_6);
-divRow1.appendChild(div1_7);divRow1.appendChild(div1_8);
-divRow1.appendChild(div1_9);divRow1.appendChild(div1_10);
-divRow1.appendChild(div1_11);divRow1.appendChild(div1_12);
-divRow1.appendChild(div1_13);divRow1.appendChild(div1_14);
-divRow1.appendChild(div1_15);divRow1.appendChild(div1_16);
-
-
-const divrow3=document.createElement('span');
-divrow3.classList.add('divrow3')
-container.appendChild(divrow3);
-
-const div3_1=document.createElement('div');const div3_2=document.createElement('div');
-const div3_3=document.createElement('div');const div3_4=document.createElement('div');
-const div3_5=document.createElement('div');const div3_6=document.createElement('div');
-const div3_7=document.createElement('div');const div3_8=document.createElement('div');
-const div3_9=document.createElement('div');const div3_10=document.createElement('div');
-const div3_11=document.createElement('div');const div3_12=document.createElement('div');
-const div3_13=document.createElement('div');const div3_14=document.createElement('div');
-const div3_15=document.createElement('div');const div3_16=document.createElement('div'); 
-
-divrow3.appendChild(div3_1);divrow3.appendChild(div3_2);
-divrow3.appendChild(div3_3);divrow3.appendChild(div3_4);
-divrow3.appendChild(div3_5);divrow3.appendChild(div3_6);
-divrow3.appendChild(div3_7);divrow3.appendChild(div3_8);
-divrow3.appendChild(div3_9);divrow3.appendChild(div3_10);
-divrow3.appendChild(div3_11);divrow3.appendChild(div3_12);
-divrow3.appendChild(div3_13);divrow3.appendChild(div3_14);
-divrow3.appendChild(div3_15);divrow3.appendChild(div3_16);
-
-const divRow4=document.createElement('span');
-divRow4.classList.add('divrow4')
-container.appendChild(divRow4);
-
-const div4_1=document.createElement('div');const div4_2=document.createElement('div');
-const div4_3=document.createElement('div');const div4_4=document.createElement('div');
-const div4_5=document.createElement('div');const div4_6=document.createElement('div');
-const div4_7=document.createElement('div');const div4_8=document.createElement('div');
-const div4_9=document.createElement('div');const div4_10=document.createElement('div');
-const div4_11=document.createElement('div');const div4_12=document.createElement('div');
-const div4_13=document.createElement('div');const div4_14=document.createElement('div');
-const div4_15=document.createElement('div');const div4_16=document.createElement('div'); 
-
-divRow4.appendChild(div4_1);divRow4.appendChild(div4_2);
-divRow4.appendChild(div4_3);divRow4.appendChild(div4_4);
-divRow4.appendChild(div4_5);divRow4.appendChild(div4_6);
-divRow4.appendChild(div4_7);divRow4.appendChild(div4_8);
-divRow4.appendChild(div4_9);divRow4.appendChild(div4_10);
-divRow4.appendChild(div4_11);divRow4.appendChild(div4_12);
-divRow4.appendChild(div4_13);divRow4.appendChild(div4_14);
-divRow4.appendChild(div4_15);divRow4.appendChild(div4_16);
-
-const divRow5=document.createElement('span');
-divRow5.classList.add('divrow5')
-container.appendChild(divRow5);
-
-const divRow6=document.createElement('span');
-divRow6.classList.add('divrow6')
-container.appendChild(divRow6);
-
-const divRow7=document.createElement('span');
-divRow7.classList.add('divrow7')
-container.appendChild(divRow7);
-
-
-const divRow8=document.createElement('span');
-divRow8.classList.add('divrow8')
-container.appendChild(divRow8);
-
-const divRow9=document.createElement('span');
-divRow9.classList.add('divrow9')
-container.appendChild(divRow9);
-
-
-const divRow10=document.createElement('span');
-divRow10.classList.add('divrow10')
-container.appendChild(divRow10);
-
-const divRow11=document.createElement('span');
-divRow11.classList.add('divrow11')
-container.appendChild(divRow11);
-
-const divRow12=document.createElement('span');
-divRow12.classList.add('divrow12')
-container.appendChild(divRow12);
-
-const divRow13=document.createElement('span');
-divRow13.classList.add('divrow13')
-container.appendChild(divRow13);
-
-
-const divRow14=document.createElement('span');
-divRow14.classList.add('divrow14')
-container.appendChild(divRow14);
-
-
-const divRow15=document.createElement('span');
-divRow15.classList.add('divrow15')
-container.appendChild(divRow15);
-
-const divRow16=document.createElement('span');
-divRow16.classList.add('divrow16')
-container.appendChild(divRow16);
+gridMaker(rows)
