0ff5e234d935b93d61b50dc30bc64196f202e708
Navwojah-Ariko_Eloho <arikoeloho1996@gmail.com>
2022-07-23 09:35:39 +0100

    feat: add button for creating the grid

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,50 @@
+// let gridSelect=Number(prompt('How many squares do you want per side?'));
+// function colorMe() {
+//     // let rowDiv=document.querySelector('.rowDiv')
+//     const btnDiv=document.querySelector('.btnDiv');
+//     colorBtn.textContent='Color Picker';
+//     colorBtn.addEventListener('click', () => {
+//         rows.forEach( row => row.addEventListener("mouseover", () => {
+//             let R=Math.floor(Math.random()*255);
+//             let G=Math.floor(Math.random()*255);
+//             let B=Math.floor(Math.random()*255);
+//             row.style.background= `rgb(${R}, ${G}, ${B})`
+//         })   
+//         );
+//     })
+//     btnDiv.append(colorBtn); 
+// }
+
+// colorMe()
+// for (let x=0; x<gridSelect;x++) {
+//     const column=document.createElement('div');
+//     column.classList.add("columnDiv");
+//     for (let y=0;y<gridSelect;y++) {
+//         const rows=document.createElement('div');
+//         rows.classList.add("rowDiv");
+//         column.append(rows)
+//     }
+//     containerDiv.append(column);
+// }
 const body=document.querySelector('body');
 const containerDiv=document.querySelector('.container')
-let gridSelect=Number(prompt('How many squares do you want per side?'));
-
-for (let x=0; x<gridSelect;x++) {
-    const column=document.createElement('div');
-    column.classList.add("columnDiv");
-    for (let y=0;y<gridSelect;y++) {
-        const rows=document.createElement('div');
-        rows.classList.add("rowDiv");
-        column.append(rows)
+document.querySelector("button").addEventListener("click", () => {
+    let gridSelect=Number(prompt('How many squares do you want per side?'));
+    containerDiv.innerHTML='';
+    for (let x=0; x<gridSelect;x++) {
+        const column=document.createElement('div');
+        column.classList.add("columnDiv");
+        for (let y=0;y<gridSelect;y++) {
+            const rows=document.createElement('div');
+            rows.classList.add("rowDiv");
+            column.append(rows)
+        }
+        containerDiv.append(column);
     }
-    containerDiv.append(column);
-}
-const colorBtn=document.createElement('button')
+    
+})
 
-function colorMe() {
-    // let rowDiv=document.querySelector('.rowDiv')
-    const btnDiv=document.querySelector('.btnDiv');
-    colorBtn.textContent='Color Picker';
-    colorBtn.addEventListener('click', () => {
-        rows.forEach( row => row.addEventListener("mouseover", () => {
-            let R=Math.floor(Math.random()*255);
-            let G=Math.floor(Math.random()*255);
-            let B=Math.floor(Math.random()*255);
-            row.style.background= `rgb(${R}, ${G}, ${B})`
-        })   
-        );
-    })
-    btnDiv.append(colorBtn); 
-}
 
-colorMe()
+
+
 
