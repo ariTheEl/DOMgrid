e5fdc77fe728af113cae7ee35e93d62465309934
Navwojah-Ariko_Eloho <arikoeloho1996@gmail.com>
2022-07-29 02:01:54 +0100

    feat:add random color generator

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,6 @@
-// let gridSelect=Number(prompt('How many squares do you want per side?'));
-// colorMe()
-// for (let x=0; x<gridSelect;x++) {
-//     const column=document.createElement('div');
-//     column.classList.add("columnDiv");
-//     for (let y=0;y<gridSelect;y++) {
-//         const rows=document.createElement('div');
-//         rows.classList.add("rowDiv");
-//         column.append(rows)
-//     }
-//     containerDiv.append(column);
-// }
-
-// function colorMe() {
-//     // let rowDiv=document.querySelector('.rowDiv')
-//     // const btnDiv=document.querySelector('.btnDiv');
-//     colorBtn.textContent='Color Picker';
-//     colorBtn.addEventListener('click', () => {
-//         rows.forEach( row => row.addEventListener("mouseover", () => {
-//             let R=Math.floor(Math.random()*255);
-//             let G=Math.floor(Math.random()*255);
-//             let B=Math.floor(Math.random()*255);
-//             row.style.background= `rgb(${R}, ${G}, ${B})`
-//         })   
-//         );
-//     })
-//     btnDiv.append(colorBtn); 
-// }
-
+let R=Math.random()*255;
+let G=Math.random()*255;
+let B=Math.random()*255;
 
 const body=document.querySelector('body');
 const containerDiv=document.querySelector('.container')
@@ -39,14 +13,16 @@ document.querySelector("button").addEventListener("click", () => {
         for (let y=0;y<gridSelect;y++) {
             const rows=document.createElement('div');
             rows.classList.add("rowDiv");
+
             rows.addEventListener("mouseover", () => {
-                rows.style.backgroundColor="#00abee"
+                rows.style.background=`rgb(${R}, ${G}, ${B})`
             })
             column.append(rows)
         containerDiv.append(column);
     }
     
 }})
+
 
 
 
